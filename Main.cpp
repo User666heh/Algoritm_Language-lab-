8bd921f058fcbf39a53fc7e170231ec90d4028cf
User666heh <103883464+User666heh@users.noreply.github.com>
2023-03-16 21:40:47 +0300

    Добавьте файлы проекта.

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int Zum(int a, int b)
+{
+	int c = a + b;
+	return 88;
+}
+
+int main()
+{ 		
+	std::string name;
+
+	name = "Kirll";
+
+	int a = 5;
+
+	int b = 6;
+
+	int c;
+
+	c = Zum(a, b);
+
+	cout << c << endl;
+
+	cout << name << "\n";
+
+	cout << "Hello world";
+}
